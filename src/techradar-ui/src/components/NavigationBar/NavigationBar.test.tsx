4d589ac67731bf8565dbe4f2f2226d7de7321940
Matt Gerega <gerega@gmail.com>
2024-01-31 15:44:31 -0500

    Updated unit tests for the UI #2

--- src/techradar-ui/src/components/NavigationBar/NavigationBar.test.tsx
+++ src/techradar-ui/src/components/NavigationBar/NavigationBar.test.tsx
@@ -1,15 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import { NavigationBar } from "./NavigationBar";
 
 describe("NavigationBar", () => {
-  it("Renders Navigation Bar", () => {
+  it("Renders Navigation Bar", async () => {
     const navBar = render(
       <NavigationBar brand="test" />
     );
-    screen.debug();
-    //const span = navBar.findByRole('navigation');
-    //expect(span.text()).toBe("test");
-    expect(true).toBe(true);
+
+    const item = await navBar.findByText("test");
+    expect(item).toHaveClass("navbar-brand");
   });
 });
